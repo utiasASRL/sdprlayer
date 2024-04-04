4dbc35df41ee20f001943f89b4abe96018d0fb03
Frederike Duembgen <frederike.duembgen@gmail.com>
2024-04-04 15:42:34 +0200

    Fix RO test

--- _test/test_ro_bias.py
+++ _test/test_ro_bias.py
@@ -1,10 +1,10 @@
 import os
 
+from cert_tools.linalg_tools import rank_project
 import matplotlib.pylab as plt
 import numpy as np
 import scipy.sparse as sp
 import torch
-from cert_tools.eopt_solvers import opts_cut_dflt, solve_eopt
 
 from sdprlayer import SDPRLayer
 
@@ -20,41 +20,43 @@ class RangeOnlyProblem(object):
         self.d = d
 
         self.landmarks = np.random.uniform(low=-5, high=5, size=(n_landmarks, d))
-        self.biases = np.random.uniform(size=n_landmarks)  # between 0 and 1
+        self.biases = 0.1 * np.arange(n_landmarks)  # easier for debuggin
+        # self.biases = np.random.uniform(size=n_landmarks)  # between 0 and 1
         self.positions = np.zeros((n_positions, d))
-        self.gt_distances = (
-            np.linalg.norm(
-                self.landmarks[None, :, :] - self.positions[:, None, :], axis=2
-            )
-            + self.biases[None, :]
-        )  # n_positions x n_landmarks distance matrix
-        self.distances = self.gt_distances + np.random.normal(
+        self.gt_distances = np.linalg.norm(
+            self.landmarks[None, :, :] - self.positions[:, None, :], axis=2
+        )
+        # n_positions x n_landmarks distance matrix
+        self.biased_distances = self.gt_distances + self.biases[None, :]
+        self.biased_distances = self.biased_distances + np.random.normal(
             scale=noise, loc=0, size=self.gt_distances.shape
         )
 
     def build_data_mat(self, biases):
         Q_tch = torch.zeros((self.d + 2, self.d + 2), dtype=torch.double)
 
-        alphas = (torch.Tensor(self.distances) - biases) ** 2 - torch.Tensor(
-            np.linalg.norm(self.landmarks, axis=1) ** 2
-        )
+        landmarks_tensor = torch.Tensor(self.landmarks)  # K x d
+        alphas = (
+            torch.Tensor(self.biased_distances) - biases
+        ) ** 2 - landmarks_tensor.norm(p=2, dim=1) ** 2
         # Structure of Q:
         # sum_i (
         # [ alpha_i**2  2*alpha_i*a_i'  -alpha_i ] 1
         # [    *        4*a_i*a_i'      -2*a_i   ] d
         # [    *             *             1     ] 1
         # )
-        landmarks_tensor = torch.Tensor(self.landmarks)
         Q_tch[0, 0] = torch.sum(alphas**2)
         a = 2 * torch.sum(alphas * landmarks_tensor.T, axis=1)
         Q_tch[0, 1 : self.d + 1] = a
         Q_tch[1 : self.d + 1, 0] = a
         Q_tch[0, self.d + 1] = -torch.sum(alphas)
-        Q_tch[1 : self.d + 1, 1 : self.d + 1] = torch.sum(
+        Q_tch[self.d + 1, 0] = -torch.sum(alphas)
+        Q_tch[1 : self.d + 1, 1 : self.d + 1] = 4 * torch.sum(
             landmarks_tensor[:, :, None] @ landmarks_tensor[:, None, :],
             axis=0,
         )  # (K x d x 1) @ (K x 1 x d) = (K x d x d)
-        Q_tch[1 : self.d + 1, self.d + 1] = torch.sum(landmarks_tensor, axis=0)
+        Q_tch[1 : self.d + 1, self.d + 1] = -2 * torch.sum(landmarks_tensor, axis=0)
+        Q_tch[self.d + 1, 1 : self.d + 1] = -2 * torch.sum(landmarks_tensor, axis=0)
         Q_tch[self.d + 1, self.d + 1] = self.n_landmarks
         return Q_tch
 
@@ -86,54 +88,27 @@ def get_prob_data(noise=1e-3):
     return prob, constraints
 
 
-def local_solver(p: torch.Tensor, x_init=0.0):
-    # Detach parameters
-    biasess = p.cpu().detach().double().numpy()
-    # Simple gradient descent solver
-    grad_tol = 1e-10
-    max_iters = 200
-    n_iter = 0
-    alpha = 1e-2
-    grad_sq = np.inf
-    x = x_init
-    while grad_sq > grad_tol and n_iter < max_iters:
-        # compute polynomial gradient
-        p_deriv = np.array([p * i for i, p in enumerate(biasess)])[1:]
-        grad = np.polyval(p_deriv[::-1], x)
-        grad_sq = grad**2
-        # Descend
-        x = x - alpha * grad
-    # Convert to expected vector form
-    x_hat = np.array([1, x, x**2, x**3])[:, None]
-    return x_hat
-
-
-def certifier(objective, constraints, x_cand):
-    opts = opts_cut_dflt
-    method = "cuts"
-    _, output = solve_eopt(
-        Q=objective, Constraints=constraints, x_cand=x_cand, opts=opts, method=method
-    )
-    if not output["status"] == "POS_LB":
-        raise ValueError("Unable to certify solution")
-    # diffcp assumes the form:  H = Q - A*mult
-    return output["H"], -output["mults"]
-
-
-def test_inner():
-    """Make sure we get the correct position when using the ground truth biases."""
-    from cert_tools.linalg_tools import rank_project
+def test_toy_inner(noise=0, verbose=False):
+    """Make sure we get the (almost) perfect position when using ground truth biases."""
 
     np.random.seed(2)
     # Get data from data function
-    prob, constraints = get_prob_data(noise=0)
+    prob, constraints = get_prob_data(noise=noise)
     # Create SDPR Layer
-    optlayer = SDPRLayer(n_vars=prob.d + 2, constraints=constraints, use_dual=False)
+    optlayer = SDPRLayer(
+        objective=None, n_vars=prob.d + 2, constraints=constraints, use_dual=False
+    )
 
     # Use perfect biases.
     values = prob.biases
     p = torch.tensor(values, requires_grad=True)
-    sdp_solver_args = {"eps": 1e-9, "verbose": True}
+
+    if noise == 0:
+        Q = prob.build_data_mat(p).detach().numpy()
+        x_gt = np.hstack([1.0, np.zeros(prob.d + 1)])
+        assert abs(x_gt.T @ Q @ x_gt) < 1e-10
+
+    sdp_solver_args = {"eps": 1e-9, "verbose": verbose}
     X, x_select = optlayer(prob.build_data_mat(p), solver_args=sdp_solver_args)
     x_select = x_select.detach().numpy().flatten()
     positions_est = x_select[: prob.d]
@@ -141,27 +116,28 @@ def test_inner():
     # testing
     x_project, info = rank_project(X.detach().numpy(), 1)
     x_project = x_project.flatten()
-    assert info["error eigs"] < 1e-8, "not rank 1!"
-    assert abs(x_project[0] - 1) < 1e-10, f"not homogeneous! {x_project[0]}!=1"
+    assert info["error eigs"] < 1e-7, "not rank 1!"
+    assert abs(x_project[0] - 1) < 1e-7, f"not homogeneous! {x_project[0]}!=1"
     np.testing.assert_almost_equal(
         x_select,
         x_project[1:],
         decimal=6,
         err_msg="selection not equal to projection!",
     )
+    decimal = 6 if noise == 0 else abs(round(np.log10(noise)))
     np.testing.assert_almost_equal(
         positions_est,
         prob.positions.flatten(),
-        decimal=6,
+        decimal=decimal,
         err_msg="did not converge to ground truth",
     )
 
 
-def test_outer(display=False):
-    """Make sure that we converge to the correct biases when using ground truth distances."""
+def test_toy_outer(noise=0, verbose=False):
+    """Make sure that we converge to the (almost) perfect biases when using (almost) perfect distances."""
     np.random.seed(2)
     # Get data from data function
-    prob, constraints = get_prob_data(noise=0)
+    prob, constraints = get_prob_data(noise=noise)
 
     # Create SDPR Layer
     optlayer = SDPRLayer(n_vars=4, constraints=constraints)
@@ -183,44 +159,52 @@ def test_outer(display=False):
         )
         return loss, X
 
-    # Define Optimizer
-    opt = torch.optim.Adam(params=[p], lr=1e-2)
+    opt = torch.optim.Adam(params=[p], lr=1e-3)
+
     # Execute iterations
     losses = []
-    max_iter = 120
-    X_init = None
-    n_iter = 0
+    max_iter = 1000
+    grad_norm_tol = 1e-7
     loss_val = np.inf
-    while loss_val > 1e-4 and n_iter < max_iter:
+    p_grad_norm = np.inf
+    converged = False
+    for n_iter in range(max_iter):
         # Update Loss
         opt.zero_grad()
         loss, sol = gen_loss(p)
-        if n_iter == 0:
-            X_init = sol.cpu().detach().numpy()
+
         # run optimizer
         loss.backward(retain_graph=True)
+        p_grad_norm = p.grad.norm(p=2)
+        biases = p.detach().numpy().round(3)
+        if p_grad_norm < grad_norm_tol:
+            msg = f"converged in grad after {n_iter} iterations."
+            converged = True
+            break
         opt.step()
         loss_val = loss.item()
         losses.append(loss_val)
-        x_min = sol.detach().numpy()[0, 1]
         n_iter += 1
-        if display:
-            print(f"min:\t{x_min}\tloss:\t{losses[-1]}")
-    if display:
-        print(f"iterations: \t{n_iter}")
-    # Check the rank of the solution
-    X_new = sol.detach().numpy()
-    evals_new = np.sort(np.linalg.eigvalsh(X_new))[::-1]
-    evr_new = evals_new[0] / evals_new[1]
-    if display:
-        print(f"new EVR:\t{evr_new}")
+        if verbose and ((n_iter % 10 == 0) or converged):
+            print(
+                f"{n_iter}: biases:\t{biases}\tgrad norm:\t{p_grad_norm:.2e}\tloss:\t{losses[-1]:.2e}"
+            )
+    if not converged:
+        msg = f"did not converge in {n_iter} iterations"
+    print(msg)
 
-    # Assert that optimization worked as expected.
-    assert n_iter < 120, "Terminated on max iterations"
-    assert loss_val <= 1e-4, "Loss did not drop to expected value"
-    assert np.log10(evr_new) >= 9, "Solution is not Rank-1"
+    decimal = 3 if noise == 0 else 1
+    np.testing.assert_almost_equal(
+        biases,
+        prob.biases,
+        decimal=decimal,
+        err_msg="bias did not converge to ground truth",
+    )
 
 
 if __name__ == "__main__":
-    test_inner()
-    test_outer()
+    test_toy_inner(noise=0)
+    test_toy_outer(noise=0, verbose=True)
+
+    test_toy_inner(noise=1e-3)
+    test_toy_outer(noise=1e-3, verbose=True)
