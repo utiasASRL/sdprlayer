d58627ffee42ac8cb20cf77def5c561826679f90
Frederike Duembgen <frederike.duembgen@gmail.com>
2024-04-05 11:57:51 +0200

    Add ro_certs and more realistic Q creation

--- _test/test_ro_bias.py
+++ _test/test_ro_bias.py
@@ -1,14 +1,18 @@
-import os
-
-from cert_tools.linalg_tools import rank_project
 import matplotlib.pylab as plt
 import numpy as np
 import scipy.sparse as sp
 import torch
+from cert_tools.linalg_tools import rank_project
 
+from ro_certs.problem import Problem, Reg
+from ro_certs.sdp_setup import get_A_list, get_Q_matrix, get_R_matrix
 from sdprlayer import SDPRLayer
 
-root_dir = os.path.abspath(os.path.dirname(__file__) + "/../")
+torch.set_default_dtype(torch.float64)
+
+N_LANDMARKS = 5
+N_POSITIONS = 1
+D = 2
 
 
 class RangeOnlyProblem(object):
@@ -33,18 +37,22 @@ class RangeOnlyProblem(object):
         )
 
     def build_data_mat(self, biases):
+        """build simple data matrix with bias
+        Structure of Q:
+        sum_i (
+        [ alpha_i**2  2*alpha_i*a_i'  -alpha_i ] 1
+        [    *        4*a_i*a_i'      -2*a_i   ] d
+        [    *             *             1     ] 1
+        )
+        where alpha is:
+        alpha = (d - b)**2 - a**2 /// d**2 - a**2  # -2*b*d + b**2
+        """
         Q_tch = torch.zeros((self.d + 2, self.d + 2), dtype=torch.double)
 
         landmarks_tensor = torch.Tensor(self.landmarks)  # K x d
         alphas = (
             torch.Tensor(self.biased_distances) - biases
         ) ** 2 - landmarks_tensor.norm(p=2, dim=1) ** 2
-        # Structure of Q:
-        # sum_i (
-        # [ alpha_i**2  2*alpha_i*a_i'  -alpha_i ] 1
-        # [    *        4*a_i*a_i'      -2*a_i   ] d
-        # [    *             *             1     ] 1
-        # )
         Q_tch[0, 0] = torch.sum(alphas**2)
         a = 2 * torch.sum(alphas * landmarks_tensor.T, axis=1)
         Q_tch[0, 1 : self.d + 1] = a
@@ -67,20 +75,17 @@ class RangeOnlyProblem(object):
 
 
 def get_prob_data(noise=1e-3):
-    n_landmarks = 5
-    n_positions = 1
-    d = 2
     # Define landmarks
     np.random.seed(0)
     prob = RangeOnlyProblem(
-        n_landmarks=n_landmarks, n_positions=n_positions, d=d, noise=noise
+        n_landmarks=N_LANDMARKS, n_positions=N_POSITIONS, d=D, noise=noise
     )
 
     # homogenizing constraint not included here as it is added by the layer
     constraints = []
     # x is of shape [h, x', z]
-    A = sp.csc_array((d + 2, d + 2))  # z = ||x||^2
-    for i in range(d):
+    A = sp.csc_array((D + 2, D + 2))  # z = ||x||^2
+    for i in range(D):
         A[i + 1, i + 1] = 1
     A[0, -1] = -0.5
     A[-1, 0] = -0.5
@@ -134,7 +139,9 @@ def test_toy_inner(noise=0, verbose=False):
 
 
 def test_toy_outer(noise=0, verbose=False):
-    """Make sure that we converge to the (almost) perfect biases when using (almost) perfect distances."""
+    """Make sure that we converge to the (almost) perfect biases when using
+    (almost) perfect distances.
+    """
     np.random.seed(2)
     # Get data from data function
     prob, constraints = get_prob_data(noise=noise)
@@ -187,7 +194,7 @@ def test_toy_outer(noise=0, verbose=False):
         n_iter += 1
         if verbose and ((n_iter % 10 == 0) or converged):
             print(
-                f"{n_iter}: biases:\t{biases}\tgrad norm:\t{p_grad_norm:.2e}\tloss:\t{losses[-1]:.2e}"
+                f"{n_iter}: biases: {biases}\tgrad norm: {p_grad_norm:.2e}\tloss: {losses[-1]:.2e}"
             )
     if not converged:
         msg = f"did not converge in {n_iter} iterations"
@@ -202,9 +209,116 @@ def test_toy_outer(noise=0, verbose=False):
     )
 
 
-if __name__ == "__main__":
-    test_toy_inner(noise=0)
-    test_toy_outer(noise=0, verbose=True)
+def add_biases_to_Q(Q_torch: torch.Tensor, prob: Problem, biases: torch.Tensor):
+    """Change Q to be parametrized with unknon biases.
 
-    test_toy_inner(noise=1e-3)
-    test_toy_outer(noise=1e-3, verbose=True)
+    In this example, we use a weighted norm with weights Sn
+
+    The new strcuture of each Q block should be:
+    hn  [ alphan.T @ Sn @ alphan  2*alphan.T @ Sn @ An -alphan.T @ Sn @ 1 ] 1
+    xn  [    *                      4*An.T @ Sn @ An   -2 @ An.T @ Sn @ 1 ] d
+    zn  [    *                *                            1.t @ Sn @ 1   ] 1
+    where:
+    alphan = (dn - bn)**2 - an**2, inR(K)
+         [a1.T]
+    An = [... ]  inR(K x d)
+         [aK.T]
+    So we just need to change the structure of the last column/row of Q!
+
+    Taking into account the correct order etc, the new row should have the structure:
+     x1                                z1          ...   xN   zN    h
+    [2*alpha1.T @ S1 @ An     alpha1.T @ S1 @ 1   ...   ...  ...  sum_n alphan.T @ Sn @ alphan]
+    """
+    distances = torch.Tensor(np.sqrt(prob.D_noisy_sq))
+    landmarks = torch.Tensor(prob.anchors)  # n_landmarks x d
+    S_all = torch.Tensor(prob.Sig_inv)
+    E = np.sum(prob.W)
+
+    # we want alpha to be n_landmarks x n_positions
+    alpha = (distances - biases[:, None]) ** 2 - (
+        torch.norm(landmarks, p=2, dim=1) ** 2
+    )[:, None]
+
+    Q_torch[-1, -1] = 0
+    dim = prob.get_dim() + 1
+    for n in range(prob.N):
+        nnz = prob.W[:, n] != 0
+        S = S_all[nnz, :][:, nnz]
+        # xi element
+        new_xi = 2 * alpha[nnz, n].T @ S @ landmarks[nnz, :] / E
+        Q_torch[-1, n * dim : n * dim + prob.d] = new_xi
+        Q_torch[n * dim : n * dim + prob.d, -1] = new_xi
+        # zi element
+        new_zi = -torch.sum(alpha[nnz, n].T @ S) / E
+        Q_torch[-1, (n + 1) * dim - 1 : (n + 1) * dim] = new_zi
+        Q_torch[(n + 1) * dim - 1 : (n + 1) * dim, -1] = new_zi
+        # h element
+        Q_torch[-1, -1] += alpha[nnz, n].T @ S @ alpha[nnz, n] / E
+    return Q_torch
+
+
+def add_sigma_to_R(R, prob, sigma):
+    raise NotImplementedError("parametrizing sigma is not implemented yet.")
+
+
+def build_data_mat(prob, biases=None, sigma_acc_est=None):
+    # build data matrix, introducing unknown biases.
+    R_old = torch.Tensor(get_R_matrix(prob).toarray())
+    Q_old = torch.Tensor(get_Q_matrix(prob).toarray())
+    if biases is not None:
+        Q_new = add_biases_to_Q(Q_old, prob, biases)
+        return Q_new + R_old
+    elif sigma_acc_est is not None:
+        R_new = add_sigma_to_R(R_old)
+        return Q_old + R_new
+    else:
+        raise ValueError("must either give biases or sigma_acc_est")
+
+
+def test_ro_certs(noise=0):
+    np.random.seed(1)
+    n_landmarks = 5
+    n_positions = 4
+    d = 2
+    prob = Problem(
+        N=n_positions,
+        d=d,
+        K=n_landmarks,
+        regularization=Reg.CONSTANT_VELOCITY,
+    )
+    prob.generate_random(sigma_dist_real=noise)
+    # matrices without biases
+    R = get_R_matrix(prob)
+    Q = R + get_Q_matrix(prob)
+
+    # check that zero bias gives the same result.
+    biases = np.zeros(n_landmarks)
+    prob.add_biases(biases)
+    prob.D_noisy_sq = prob.D_biased**2
+
+    biases = torch.Tensor(biases)
+    Q_new = build_data_mat(prob, biases).detach().numpy()
+    np.testing.assert_allclose(Q_new, Q.toarray(), atol=1e-6)
+
+    # create problem with biases
+    biases = np.random.uniform(size=n_landmarks)
+    prob.add_biases(biases)
+    prob.D_noisy_sq = prob.D_biased**2
+
+    # create matrix where we remove the biases
+    biases = torch.Tensor(biases)
+    Q_new = build_data_mat(prob, biases).detach().numpy()
+    np.testing.assert_allclose(Q_new, Q.toarray())
+
+    # TODO: continue here
+    # A_0, A_list = get_A_list(prob)
+
+
+if __name__ == "__main__":
+    # test_toy_inner(noise=0)
+    # test_toy_outer(noise=0, verbose=True)
+
+    # test_toy_inner(noise=1e-3)
+    # test_toy_outer(noise=1e-3, verbose=True)
+
+    test_ro_certs()
