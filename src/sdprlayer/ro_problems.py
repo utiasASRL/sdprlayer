Converging example (but to wrong values)

src/sdprlayer/ro_problems.py
@@ -24,7 +24,6 @@ class RealProblem(Problem):
         real_prob.anchors = prob.anchors
         real_prob.D_noisy_sq = prob.D_noisy_sq
         real_prob.W = prob.W
-        real_prob.positions = prob.trajectory
         real_prob.trajectory = prob.trajectory
         real_prob.times = prob.times
         real_prob.biases = real_prob.get_biases(real_prob.D_noisy_sq, squared=True)
@@ -45,8 +44,9 @@ class RealProblem(Problem):
         other.N = number
         other.D_noisy_sq = self.D_noisy_sq[:, keep_idx]
         other.W = self.W[:, keep_idx]
-        other.positions = self.positions[keep_idx, :]
+        other.trajectory = self.trajectory[keep_idx, :]
         other.times = self.times[keep_idx]
+        self.trajectory = None
         return other
 
     def __init__(
@@ -68,7 +68,6 @@ class RealProblem(Problem):
         self.n_calib = n_calib if n_calib else n_anchors
         if not real:
             self.generate_random(sigma_dist_real=noise)
-            self.positions = self.trajectory
 
     def generate_biases(self, biases=None):
         if biases is None:
@@ -198,11 +197,11 @@ class ToyProblem(object):
         self.biases[: self.n_calib] = 0.1 * np.arange(1, self.n_calib + 1)
         # self.biases[: self.n_calib] = np.random.uniform(size=n_calib)  # between 0 and 1
 
-        # self.positions = np.random.uniform(low=-1, high=1, size=(n_positions, d))
-        self.positions = np.mean(self.anchors, axis=0)[None, :]
+        # self.trajectory = np.random.uniform(low=-1, high=1, size=(n_positions, d))
+        self.trajectory = np.mean(self.anchors, axis=0)[None, :]
 
         self.gt_distances = np.linalg.norm(
-            self.anchors[None, :, :] - self.positions[:, None, :], axis=2
+            self.anchors[None, :, :] - self.trajectory[:, None, :], axis=2
         )
         # n_positions x n_anchors distance matrix
         self.biased_distances = self.gt_distances + self.biases[None, :]
@@ -212,7 +211,7 @@ class ToyProblem(object):
 
     def get_x(self):
         return np.hstack(
-            [1.0, self.positions.flatten(), np.linalg.norm(self.positions) ** 2]
+            [1.0, self.trajectory.flatten(), np.linalg.norm(self.trajectory) ** 2]
         )
 
     def get_positions(self, x):
@@ -266,4 +265,4 @@ class ToyProblem(object):
     def plot_problem(self):
         fig, ax = plt.subplots()
         ax.scatter(*self.landmarks[:, :2].T)
-        ax.scatter(*self.positions[:, :2].T)
+        ax.scatter(*self.trajectory[:, :2].T)

src/sdprlayer/ro_tuner.py
@@ -26,7 +26,10 @@ def update_err_plot(ax, idx, errors, labels=False):
 
 
 def update_pos_plot(ax, pos):
-    ax.scatter(*pos[:2, :], s=1)
+    if pos.shape[1] >= 1:
+        ax.plot(*pos[:2, :], marker="o")
+    else:
+        ax.scatter(*pos[:2, :], s=1)
 
 
 def setup_error_plots(target_loss):
@@ -42,7 +45,10 @@ def setup_error_plots(target_loss):
 
 def setup_position_plot(prob):
     fig_pos, ax_pos = plt.subplots()
-    ax_pos.scatter(*prob.positions[:, : prob.d].T, color="k")
+    if prob.trajectory.shape[0] > 1:
+        ax_pos.plot(*prob.trajectory[:, : prob.d].T, color="k", marker="o")
+    else:
+        ax_pos.scatter(*prob.trajectory[:, : prob.d].T, color="k")
     ax_pos.scatter(*prob.anchors[:, : prob.d].T, marker="x", color="k")
     return fig_pos, ax_pos
 
@@ -75,7 +81,7 @@ def run_calibration(
         if not (evr > 1e6):
             print("Warning: solution not rank 1:", eigs[-5:])
         positions = prob.get_positions(x)
-        loss = torch.norm(positions - torch.tensor(prob.positions))
+        loss = torch.norm(positions - torch.tensor(prob.trajectory))
         return loss, positions
 
     # opt = torch.optim.Adam(params=[p], lr=1e-2, eps=1e-10, weight_decay=0.2)